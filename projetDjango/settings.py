16c303f87ad9ec01aae62f0185c40cf7dfee285b
martin <martinv9663@gmail.com>
2025-06-02 00:04:39 +0200

    modifs

--- projetDjango/settings.py
+++ projetDjango/settings.py
@@ -30,6 +30,34 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+# Detect environment (default to 'local' if variable ENVIRONMENT is not set)
+ENVIRONMENT = os.getenv('ENVIRONMENT', 'local')
+
+
+# Database
+# https://docs.djangoproject.com/en/5.2/ref/settings/#databases
+
+if ENVIRONMENT == 'production':
+    # PostgreSQL config for Render (ou autre prod)
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': os.getenv('POSTGRES_DB'),
+            'USER': os.getenv('POSTGRES_USER'),
+            'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
+            'HOST': os.getenv('POSTGRES_HOST'),
+            'PORT': os.getenv('POSTGRES_PORT', '5432'),
+        }
+    }
+else:
+    # SQLite en local
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
+
 
 # Application definition
 
@@ -86,16 +114,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'projetDjango.wsgi.application'
 
-
-# Database
-# https://docs.djangoproject.com/en/5.2/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 
 
 # Password validation
