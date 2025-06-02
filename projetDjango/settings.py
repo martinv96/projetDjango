9e7edb2c17e14a3b2f69f3a9ac6313ca3df4989b
martin <martinv9663@gmail.com>
2025-06-02 16:11:11 +0200

    settings

--- projetDjango/settings.py
+++ projetDjango/settings.py
@@ -83,7 +83,6 @@ INTERNAL_IPS = [
 NPM_BIN_PATH = "C:/Program Files/nodejs/npm.cmd"
 
 
-
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
