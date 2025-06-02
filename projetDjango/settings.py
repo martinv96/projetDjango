fa113c96fef70000b4b60f32c327f116cef348ba
martin <martinv9663@gmail.com>
2025-06-02 15:56:12 +0200

    settigns

--- projetDjango/settings.py
+++ projetDjango/settings.py
@@ -165,8 +165,8 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = config('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+EMAIL_HOST_USER = config('EMAIL_HOST_USER', default='test@example.com')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD', default='test')
 EMAIL_USE_TLS = True
 DEFAULT_FROM_EMAIL = 'RunTrack <noreply@runtrack.fr>'
 
